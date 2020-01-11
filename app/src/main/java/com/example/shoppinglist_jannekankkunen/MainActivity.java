1907c4be9020caba22ab45cd0ee6cefefe71bd1f
JanneKankkunen <janneonkiva@gmail.com>
2020-01-11 16:35:49 +0200

    Jostain pitää aloittaa.

---
+++ app/src/main/java/com/example/shoppinglist_jannekankkunen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shoppinglist_jannekankkunen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
