9a5b5a993d39577cf2ab2350cb57411ea61bd1b3
JanneKankkunen <janneonkiva@gmail.com>
2020-01-13 18:17:06 +0200

    Jostain pitää aloittaa.

--- app/src/main/java/com/example/shoppinglist_jannekankkunen/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist_jannekankkunen/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.shoppinglist_jannekankkunen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText shoplist = (EditText) findViewById(R.id.editShopText);
+
+            }
+        });
+        };
     }
-}
+
